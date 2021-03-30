7718494d1ba75073099868f124ef1b43dff89747
Francisco Morais <fgmorais18@gmail.com>
2021-03-30 16:44:52 -0300

    :art: refactoring: change all .js to .tsx files and change all Classes Components to Functional Components

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,30 +1,22 @@
-import { Component } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import ReactModal from 'react-modal';
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
+interface ModalProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  children: ReactNode;
+}
 
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
-    }
-  }
+export function Modal({ isOpen, setIsOpen, children }: ModalProps) {
+  const [ modalStatus, setModalStatus ] = useState(isOpen);
 
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
+  console.log("inicio Modal -> " + isOpen);
 
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
+  useEffect(() => {
+    setModalStatus(isOpen);
+  }, [isOpen]);
 
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
-
-    return (
+  return (
       <ReactModal
         shouldCloseOnOverlayClick={!false}
         onRequestClose={setIsOpen}
@@ -51,8 +43,5 @@ class Modal extends Component {
       >
         {children}
       </ReactModal>
-    );
-  }
-};
-
-export default Modal;
+  );
+}
